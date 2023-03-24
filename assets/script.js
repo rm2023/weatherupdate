ad98d2680e3760759e8fe688a013dd04f94a1ae1
rm2023 <120582189+rm2023@users.noreply.github.com>
2023-03-24 09:09:38 -0400

    added icons to weather

--- assets/script.js
+++ assets/script.js
@@ -14,18 +14,24 @@ click1.addEventListener("click", function () {
     $('input[name="input1"]').val('');
 
     var APIKey = "1d3e7e850fece8f1faa4ded00b53eacc";
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
     fetch(queryURL).then(function (response) {
         return response.json();
         
     }).then(function (data) {
         var forecastData = data.list;
         var forecastHtml = "";
+   
         for (var i = 0; i < forecastData.length; i+=8) {
             var forecast = forecastData[i];
+            var iconCode = data.list[i].weather[0].icon;
+            var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+            var iconDescr =  data.list[i].weather[0].description;
             var date = new Date(forecast.dt_txt);
-            forecastHtml += "<div>" + date.toDateString() + "<br>" +
-                "Temperature: " + ((forecast.main.temp -273.15)*9/5+32) + " &deg;F<br>" +
+            forecastHtml += "<div class='card1'>" + date.toDateString() + "<br>" +
+            `<img src=${iconUrl} alt=${iconDescr} >` +
+                // "Temperature: " + ((forecast.main.temp -273.15)*9/5+32) + " &deg;F<br>" +
+                "Temperature: " + (forecast.main.temp) + " &deg;F<br>" +
                 "Weather description: " + forecast.weather[0].description + "</div>";
 
         }
